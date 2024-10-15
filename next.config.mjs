dab70b89c4686dcfd0457453500427a1d57b02b4
gowthamsmazz <gowthamsmart45@gmail.com>
2024-10-15 20:49:49 +0530

    intial commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true, // Optional: If using Next.js Image component
+    },
+  };
+  
+  export default nextConfig;
+  
